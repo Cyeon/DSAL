e18023ced445011ac4ece2d87a1d8ad8a5018bf3
Cyeon <77601035+Cyeon@users.noreply.github.com>
2022-06-07 13:01:28 +0900

    06/07 트리 순회 완료, 수식트리 활용 계산기 만드는 중

--- 0602_BinaryTree.cpp
+++ 0602_BinaryTree.cpp
@@ -40,3 +40,76 @@ void MakeRightSubTree(BTreeNode* main, BTreeNode* sub) {
 	}
 	main->right = sub;
 }
+
+/// <summary>
+/// 전위
+/// </summary>
+/// <param name="bt"></param>
+void PreorderTraverse(BTreeNode* bt)
+{
+	//종료 조건
+	if (bt == NULL) { return; }
+	//현재 노드 방문(출력)
+	cout << bt->data << " ";
+	//다음 방문
+	PreorderTraverse(bt->left);
+	PreorderTraverse(bt->right);
+}
+/// <summary>
+/// 중위
+/// </summary>
+/// <param name="bt"></param>
+void InorderTraverse(BTreeNode* bt)
+{
+	if (bt == NULL) { return; }
+	InorderTraverse(bt->left);
+	cout << bt->data << " ";
+	InorderTraverse(bt->right);
+}
+/// <summary>
+/// 후위
+/// </summary>
+/// <param name="bt"></param>
+void PostorderTraverse(BTreeNode* bt)
+{
+	if (bt == NULL) { return; }
+	PostorderTraverse(bt->left);
+	PostorderTraverse(bt->right);
+	cout << bt->data << " ";
+}
+BTreeNode* MakeExpTree(string exp)
+{
+	stack<BTreeNode*> stk;
+	BTreeNode* pNode = NULL;
+	for (int i = 0; i < exp.length(); i++)
+	{
+		pNode = MakeBTreeNode();
+
+		//연산자, 피연산자 체크 
+		if (isdigit(exp[i])) {
+
+		}
+		else
+		{
+
+		}
+		stk.push(pNode);
+	}
+
+
+	return pNode;
+
+}
+
+int EvaluateExpTree(BTreeNode* bt)
+{
+	// 구현해보자!
+	// 처음부터 짜기 많이 어려우면 첨부된 cpp 파일의 주석 부분 내용 채우기~!! 
+}
+
+int prec(char op) {
+	// 구현해보자!
+}
+string infixToPostfix(string infix) {
+	// 구현해보자!
+}
