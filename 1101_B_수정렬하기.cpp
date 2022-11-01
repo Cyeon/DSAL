3f0baad18ccb08cd4fc3daa97ba3d977ca39d921
Cyeon <77601035+Cyeon@users.noreply.github.com>
2022-11-02 00:46:06 +0900

    11/01 정렬
    코드 빈칸 채우기, 수 정렬하기(백준) 풀기 시작

---
+++ 1101_B_수정렬하기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	int arr[1000];
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+
+}
