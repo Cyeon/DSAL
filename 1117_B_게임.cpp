154b4fad14333f82d2f8e4196c17d06f5987d48d
Cyeon <77601035+Cyeon@users.noreply.github.com>
2022-11-22 00:45:47 +0900

    11/22 게임 문제 완료

--- 1117_B_게임.cpp
+++ 1117_B_게임.cpp
@@ -2,12 +2,29 @@
 using namespace std;
 
 int main() {
-	long long int x, y;
+	long long int x, y, result = 0;
 	cin >> x >> y;
-	if (x == y) {
+
+	int z = y * 100 / x;
+
+	if (z == 100 || z == 99) {
 		cout << -1;
-		return;
+		return 0;
 	}
 
-
+	int min = 0, max = 1000000000;
+	while (min <= max)
+	{
+		int mid = (max + min) / 2;
+		int newZ = (y + mid) * 100 / (x + mid);
+		if (newZ > z) {
+			max = mid - 1;
+		}
+		else
+		{
+			min = mid + 1;
+			result = min;
+		}
+	}
+	cout << result;
 }
